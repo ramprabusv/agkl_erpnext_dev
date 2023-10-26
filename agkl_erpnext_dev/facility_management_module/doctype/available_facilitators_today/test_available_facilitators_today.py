a18b38802a1bb4fd710785dd04a8e3452ccb5b1c
ramprabusv <ramprabusv@agnikul.in>
2023-10-26 22:26:33 +0530

    removed unwanted DTs

--- agkl_erpnext_dev/facility_management_module/doctype/available_facilitators_today/test_available_facilitators_today.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, svr and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class Testavailable_facilitators_today(FrappeTestCase):
-	pass
--- agkl_erpnext_dev/facility_management_module/doctype/exit_marking/test_exit_marking.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, svr and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class Testexit_marking(FrappeTestCase):
-	pass
