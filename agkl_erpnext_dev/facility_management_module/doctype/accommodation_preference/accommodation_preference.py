8ec855941e2e4104bf1bb64cd6bac430670140e5
ramprabusv <ramprabusv@agnikul.in>
2023-10-26 13:16:45 +0530

    some deletes

--- agkl_erpnext_dev/facility_management_module/doctype/accommodation_preference/accommodation_preference.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2023, svr and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class accommodation_preference(Document):
-	pass
--- agkl_erpnext_dev/facility_management_module/doctype/uom_materials/uom_materials.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2023, svr and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class UOM_materials(Document):
-	pass
